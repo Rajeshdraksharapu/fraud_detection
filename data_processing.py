ee98ff8df2c5ccd6df9fd9852679ab2609492d12
Rajeshdraksharapu <rajeshdraksharapu7777@gmail.com>
2021-11-25 18:40:32 +0530

    Revert "added a new funtion sum"
    This reverts commit 45b10954e24c891133981028dea7d60c64ad8893.

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,3 @@
 
 
 print("adding all the data processing code related to fraud detection")
-
-
-a=5
-b=7
-sum(a,b):
-    c=a+b
-    return a
-
-print("added a new funtion sum which is not correct")    
