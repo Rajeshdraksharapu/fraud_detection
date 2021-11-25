26d75f12c50fb86dfe180a10f2e1c6195c8e365c
Rajeshdraksharapu <rajeshdraksharapu7777@gmail.com>
2021-11-26 00:44:47 +0530

    added new ml code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,13 @@
 
 print("adding all ML related code here")
+
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print("adding code for ML model2")
+
+def model3():
+    print("adding code for ML model3")
+    
